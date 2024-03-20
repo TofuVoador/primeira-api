804e66ae4c999406e561a3cf6b9db48ca884d962
Gustavo Kumagai <iagamuk.gus@gmail.com>
2024-03-20 09:58:10 -0300

    Cors

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const userRoutes = require("./src/routes/userRoutes");
 const homeRoutes = require("./src/routes/homeRoutes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Middleware para análise de corpo JSON
+app.use(cors());
 app.use(express.json());
 
 app.use("/", homeRoutes);
